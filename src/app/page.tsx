72214bfeeec335274d7e000cd99ae53cb4716f15
cocoth <anonymousid0000000@gmail.com>
2024-04-02 14:21:51 +0700

    fixed meta tah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "@/components/footer/Footer";
-import Navbar from "@/components/navbar/Navbar";
 import LandingPage from "@/components/pages/LandingPage";
-import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <meta name="google-site-verification" content="Tu0vAsVZ_cRXOx7-AqrpePn2DosjBM5VbpOJS1jeA5s" />
+      </Head>
         <LandingPage/>
         <Footer/>
     </main>
