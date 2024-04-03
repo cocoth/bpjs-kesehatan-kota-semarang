3d5257edca5e800cefd0fc71da723f67b4cd1be2
cocoth <anonymousid0000000@gmail.com>
2024-04-03 09:40:20 +0700

    add Gtag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,18 @@ export default function Home() {
     <main>
       <Head>
         <meta name="google-site-verification" content="Tu0vAsVZ_cRXOx7-AqrpePn2DosjBM5VbpOJS1jeA5s" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PLVEW0LTSS"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+
+              gtag('config', 'G-PLVEW0LTSS');
+            `,
+          }}
+        />
       </Head>
         <LandingPage/>
         <Footer/>
