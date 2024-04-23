445ebed3966eac3b7deca060b44145c4edcea2f1
cocoth <anonymousid0000000@gmail.com>
2024-04-23 13:49:58 +0700

    payment remainder

--- next.config.mjs
+++ next.config.mjs
@@ -1,49 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // redirects: async()=>{
-    //     return[
-    //         {
-    //             source: '/',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/profile',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/faskes',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/faskes/semarang',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/faskes/demak',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/peta',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/peta/semarang',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //         {
-    //             source: '/peta/demak',
-    //             destination: '/payment',
-    //             permanent: false
-    //         },
-    //     ]
-    // }
+    redirects: async()=>{
+        return[
+            {
+                source: '/',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/profile',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/faskes',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/faskes/semarang',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/faskes/demak',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/peta',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/peta/semarang',
+                destination: '/payment',
+                permanent: false
+            },
+            {
+                source: '/peta/demak',
+                destination: '/payment',
+                permanent: false
+            },
+        ]
+    }
 };
 
 
